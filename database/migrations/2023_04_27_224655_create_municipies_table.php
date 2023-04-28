3062d6e5e32c848669a2ac3b81fbad8382041252
AlexZalez <remixsicool@gmail.com>
2023-04-27 22:39:36 -0400

    agregando columna state_id a municipies table

--- database/migrations/2023_04_27_224655_create_municipies_table.php
+++ database/migrations/2023_04_27_224655_create_municipies_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('municipies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->bigInteger('state_id');
             $table->timestamps();
         });
     }
