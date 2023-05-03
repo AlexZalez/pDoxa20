6d5c21d9e1ffa60c866cf614f999e18b0c5b3f4b
AlexZalez <remixsicool@gmail.com>
2023-05-03 00:55:43 -0400

    Agg Classroom Table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,6 +40,7 @@ class DatabaseSeeder extends Seeder
             SchemeDaySeeder::class,
             DaySeeder::class,
             UbicationSeeder::class,
+            ClassroomSeeder::class,
         ]);
     }
 }
